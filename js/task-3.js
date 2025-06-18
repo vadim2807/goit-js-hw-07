053ed561291d67169046fababf726a2f6b7d733d
LanaStudio <196567654+vadim2807@users.noreply.github.com>
2025-06-18 18:40:05 +0300

    вторая сдача

--- js/task-3.js
+++ js/task-3.js
@@ -3,8 +3,12 @@ const span = document.querySelector('#name-output');
 input.addEventListener('input', function () {
     
     let trimmedValue = input.value.trim();
-    if (input.value === "") {
+        input.value = trimmedValue;
+    
+    if (trimmedValue === "") {
         span.textContent = "Anonymous";
+    } else {
+        span.textContent = trimmedValue;
     }
    
 
